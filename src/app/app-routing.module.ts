45c82a66e5bf2c1d8e2485a741d18142f9927a3f
Tomasz Niemiec <tomaszniemiec@kodito.pl>
2022-07-01 19:52:31 +0200

    added path in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './pages/account/account.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "konto", component: AccountComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
