53bf8b49e912d5e617803e99a260fbb7a3a482f8
Tomasz Niemiec <tomaszniemiec@kodito.pl>
2022-07-03 15:06:41 +0200

    added validation for send form and added spinner for submit button

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,7 +9,25 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   logged = false;
-
+  loading = false;
+  errorText = '';
+  usersList = [
+    {
+      id: 1,
+      email: 'rekrutacja@melle.com',
+      password: 'rekrutacja'
+    },
+    {
+      id: 2,
+      email: 'biuro@test.pl',
+      password: 'test1234'
+    },
+    {
+      id: 3,
+      email: 'kontakt@test2.pl',
+      password: 'testowe987'
+    },
+  ]
   loginValues = {
     email: '',
     password: ''
@@ -17,10 +35,21 @@ export class LoginComponent implements OnInit {
   constructor( private router: Router) { }
 
   loginHandle(loginForm:NgForm){
-    console.log(loginForm)
-    window.localStorage.setItem('user',JSON.stringify(loginForm.value))
-    this.router.navigate(['/konto'])
-    this.logged = true
+      this.loading = true
+      this.errorText=''
+      setTimeout(() => {
+      const userSearch = this.usersList.filter(user => user.email === loginForm.value.email)
+      if(userSearch.length>0){
+        window.localStorage.setItem('user',JSON.stringify(loginForm.value))
+        this.router.navigate(['/konto'])
+        this.logged = true
+      }
+      else{
+        this.errorText = "Nieprawidłowy email lub hasło"
+        this.loading = false
+      }
+      
+    }, 3000);
   }
   ngOnInit(): void {
   }
